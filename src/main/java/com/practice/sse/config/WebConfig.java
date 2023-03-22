2f380841bda4d6e42395675452dcebe7324b06e1
Damgom <wisewish1@gmail.com>
2023-03-23 01:22:29 +0900

    FEAT: sse controller, sse emitters

--- src/main/java/com/practice/sse/config/WebConfig.java
+++ src/main/java/com/practice/sse/config/WebConfig.java
@@ -4,6 +4,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 public class WebConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
