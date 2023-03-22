12428e6b03beff6a313d6ac01c3cc4836bd292a8
Damgom <wisewish1@gmail.com>
2023-03-23 02:42:57 +0900

    FEAT: add annotation

--- src/main/java/com/practice/sse/sse/SseController.java
+++ src/main/java/com/practice/sse/sse/SseController.java
@@ -21,12 +21,19 @@ public class SseController {
 
     @GetMapping(value = "/connect", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public ResponseEntity<SseEmitter> connect() {
-        SseEmitter emitter = new SseEmitter();
+        /*
+        만료시간을 설정할 수 있다. default 값은 30초로 설정된다. 만료시간이 되면 브라우저에서 자동으로 서버에 재연결 요청
+         */
+        SseEmitter emitter = new SseEmitter(60 * 1000L);
         sseEmitters.add(emitter);
+        /*
+        emitter를 생성하고 나서 만료시간까지 아무런 데이터도 보내지 않으면 재연결 요청시 503 service unavailable 에러 발생할 수 있음
+        그래서 최초 연결 시 더미 데이터를 전달해주는 것이 좋다.
+         */
         try {
             emitter.send(SseEmitter.event()
-                    .name("connect")
-                    .data("connected!"));
+                    .name("connect") // 해당 이벤트의 이름을 지정
+                    .data("connected!")); // 더미데이터
         }catch (IOException e) {
             throw new RuntimeException(e);
         }
--- src/main/java/com/practice/sse/sse/SseEmitters.java
+++ src/main/java/com/practice/sse/sse/SseEmitters.java
@@ -13,13 +13,21 @@ public class SseEmitters {
 
     private final List<SseEmitter> emitters = new CopyOnWriteArrayList<>();
 
+    /*
+    SseEmitter를 생성할 때 비동기 요청이 완료되거나 타임아웃 발생 시 실행할 콜백을 등록할 수 있다.
+    타임아웃이 발생할 경우 브라우저에서 재연결 요청을 보내는데 이 때 새로운 Emitter 객체를 다시 생성하기 때문에
+    기존의 Emitter를 제거해주어야 한다.
+
+    이 콜백은 SseEmitter를 관리하는 다른 스레드에서 실행된다. 따라서 thread-safe 한 자료구조를 사용하지않으면
+    ConcurrentModificationException 이 발생할 수 있다. 그러므로 여기서는 ArrayList 가 아닌 CopyOnWriteArrayList 를 사용
+     */
     SseEmitter add(SseEmitter emitter) {
         this.emitters.add(emitter);
         log.info("new emitter added: {}", emitter);
         log.info("emitter list size: {}", emitters.size());
         emitter.onCompletion(() -> {
             log.info("onCompletion callback");
-            this.emitters.remove(emitter);
+            this.emitters.remove(emitter); // 만료되면 리스트에서 삭제한다.
         });
         emitter.onTimeout(() -> {
             log.info("onTimeout callback");
